f3e2b0e1270b420a1e7c699347ab2414cb0d4ad5
hawks70 <alfonsofalcomata57@gmail.com>
2025-12-04 09:32:56 +0100

    aggiunta id dis2

--- src/app/api/dati/route.ts
+++ src/app/api/dati/route.ts
@@ -10,7 +10,7 @@ export async function GET(req: NextRequest) {
     return NextResponse.json({ error: "Parametri mancanti" }, { status: 400 });
   }
 
-  const baseUrl = "https://script.google.com/macros/s/AKfycbzRIO30pVx-0UCtUeEDLTajD74SG17gVmkjTwl5D6hXXB9UxjQeGGfpElL9H51Q5h2o/exec";
+  const baseUrl = "https://script.google.com/macros/s/AKfycbxqdMtiEx7mHE26gWjaFaaCtBrUtmsQPeLI_RqlOcH-b6si_lAfbbrCUkRx6b3gHfPl/exec";
   const url = `${baseUrl}?email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}&distretto=${encodeURIComponent(distretto)}`;
 
   try {
