8bc061a3e2fbcdfff8a5a493e4a1bf5a03e8bc5c
hawks70 <alfonsofalcomata57@gmail.com>
2026-01-27 20:42:33 +0100

    modifica css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import '../styles/global.css';
+//import '../styles/global.css';
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import DistrettoSelector from '../components/DistrettoSelector';
